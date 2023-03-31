9fa805414ad4f9b15b4094b2949bb167352447f2
Mario Quevedo <opnfuso@gmail.com>
2023-03-30 19:15:08 -0600

    delete log

--- nodejs_server/app.js
+++ nodejs_server/app.js
@@ -20,7 +20,6 @@ var cameraArray={};
 
 //esp32cam websocket
 wss1.on('connection', function connection(ws) {
-  console.log("connection")
   ws.on('message', function incoming(message) {
     wss2.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
