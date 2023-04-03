49b8db5f05117b4f84a3b6fd9914a9649b64e2cb
Mario Quevedo <opnfuso@gmail.com>
2023-04-02 19:03:57 -0600

    delete twilio

--- app/src/renderer.ts
+++ app/src/renderer.ts
@@ -4,20 +4,3 @@
 // nodeIntegration is set to true in webPreferences.
 // Use preload.js to selectively enable features
 // needed in the renderer process.
-const accountSid = "";
-const authToken = "";
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const client = window.require("twilio")(accountSid, authToken);
-
-const callPhoneButton = document.getElementById("call-phone-button");
-
-callPhoneButton.addEventListener("click", () => {
-  // start a call to phone
-  client.calls
-    .create({
-      url: "http://demo.twilio.com/docs/voice.xml",
-      to: "",
-      from: "",
-    })
-    .then((call: { sid: any }) => console.log(call.sid));
-});
