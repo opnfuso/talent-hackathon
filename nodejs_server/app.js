36fad34289c66017873b0f22f8a82d3984527946
Mario Quevedo <opnfuso@gmail.com>
2023-04-11 20:04:37 -0600

    change to 20 seg

--- nodejs_server/app.js
+++ nodejs_server/app.js
@@ -44,7 +44,7 @@ const interval = setInterval(function ping() {
     ws.isAlive = false;
     ws.ping();
   });
-}, 30000);
+}, 20000);
 
 wss1.on("close", function close() {
   clearInterval(interval);
